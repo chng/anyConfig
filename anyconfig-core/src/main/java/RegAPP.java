c6c69de8adb053d41ad3edf6ec0531a85fcee124
alextrang <zhchngzng@gmail.com>
2015-12-31 22:06:40 +0800

    added to git

---
+++ anyconfig-core/src/main/java/RegAPP.java
@@ -0,0 +1,70 @@
+import com.avaje.ebean.annotation.CreatedTimestamp;
+import com.avaje.ebean.annotation.UpdatedTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by OurEDA on 2015/12/31.
+ */
+@Entity
+public class RegAPP {
+
+    @Id
+    String appid;
+
+    @Column
+    String addr;
+
+    @Column
+    List<RegConfig> configList;
+
+    @CreatedTimestamp
+    Date gmtCreate;
+    @UpdatedTimestamp
+    Date gmtModify;
+
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public List<RegConfig> getConfigList() {
+        return configList;
+    }
+
+    public void setConfigList(List<RegConfig> configList) {
+        this.configList = configList;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModify() {
+        return gmtModify;
+    }
+
+    public void setGmtModify(Date gmtModify) {
+        this.gmtModify = gmtModify;
+    }
+}
---
+++ anyconfig-core/src/main/java/RegConfig.java
@@ -0,0 +1,78 @@
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Created by OurEDA on 2015/12/31.
+ */
+
+public class RegConfig {
+
+    @Id
+    String id;
+
+    @Column
+    String name = "";
+    @Column
+    Serializable value = "";
+    @Column
+    String description = "";
+    @Column
+    Level level = Level.Level2;
+    @Column
+    Boolean isPersistant = true;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Serializable getValue() {
+        return value;
+    }
+
+    public void setValue(Serializable value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public Boolean getIsPersistant() {
+        return isPersistant;
+    }
+
+    public void setIsPersistant(Boolean isPersistant) {
+        this.isPersistant = isPersistant;
+    }
+
+    enum Level {
+        Level2, //一般功能级
+        Level1, //核心功能级
+        Level0; //系统级
+    }
+}
